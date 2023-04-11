5d2714a1966f520ca37b2dbbe304c277ed6dfbbe
caner-cakar <cakar_caner@outlook.com>
2023-04-11 15:44:02 +0300

    Player zıplama özelliği eklendi

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb; 
+    private BoxCollider2D coll;
 
     private float dirX = 0f;
     [SerializeField] private float moveSpeed = 7f;
-    // Start is called before the first frame update
+    [SerializeField] private float jumpSpeed = 14f;
+
+    private float jumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
+
+    [Range(1,20)]
+    public float jumpVelocity;
+
+    [SerializeField] private LayerMask jumpaleGround;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -19,5 +30,34 @@ public class PlayerMovement : MonoBehaviour
     {
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed,rb.velocity.y);
+
+        if(Input.GetButtonDown("Jump") && isGrounded())
+        {
+            isJumping=true;
+            jumpTimeCounter=jumpTime;
+            rb.velocity = Vector2.up * jumpVelocity;
+        }
+   
+        if(Input.GetButton("Jump") && isJumping == true)
+        {
+            if(jumpTimeCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpVelocity;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+        if(Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+        }
+    }
+    
+    private bool isGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center,coll.bounds.size,0f,Vector2.down,.1f,jumpaleGround);
     }
 }
